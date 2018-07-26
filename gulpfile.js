d4c809a018a51298d7209e4d5cc855d36ed54b64
TimesThree <41113047+TimesThree@users.noreply.github.com>
2018-07-26 20:23:49 +0300

    Add files via upload

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-var gulp       = require("gulp"), // Подключаем Gulp
+﻿var gulp       = require("gulp"), // Подключаем Gulp
     sass         = require("gulp-sass"), //Подключаем Sass пакет,
     browserSync  = require("browser-sync"), // Подключаем Browser Sync
     concat       = require("gulp-concat"), // Подключаем gulp-concat (для конкатенации файлов)
@@ -38,14 +38,14 @@ gulp.task("scripts", function() {
 });
 
 gulp.task("css-libs", ["sass"], function() {
-    return gulp.src("app/css/libs.css") // Выбираем файл для минификации
+    return gulp.src("app/css/main.css") // Выбираем файл для минификации
         .pipe(cssnano()) // Сжимаем
         .pipe(rename({suffix: ".min"})) // Добавляем суффикс .min
         .pipe(gulp.dest("app/css")); // Выгружаем в папку app/css
 });
 
 gulp.task("watch", ["browser-sync", "css-libs", "scripts"], function() {
-    gulp.watch("app/sass/**/*.sass", ["sass"]); // Наблюдение за sass файлами в папке sass
+    gulp.watch("app/sass/**/*.sass", ["css-libs"]); // Наблюдение за sass файлами в папке sass
     gulp.watch("app/*.html", browserSync.reload); // Наблюдение за HTML файлами в корне проекта
     gulp.watch("app/js/**/*.js", browserSync.reload);   // Наблюдение за JS файлами в папке js
 });
